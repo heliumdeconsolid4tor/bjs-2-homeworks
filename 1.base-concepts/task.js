625edc90815daea19e804c0fd6674acfe73802a7
heliumdeconsolid4tor <taniavb@bk.ru>
2023-11-17 19:36:59 +0300

    Hello! Both tasks are to be checked here. Thank you!

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,33 @@
 "use strict"
+
 function solveEquation(a, b, c) {
+
   let arr = [];
-  
+  let d = b ** 2 - 4 * a * c;
+  let x1;
+  let x2;
+
+  if (d > 0) {
+    x1 = (-b + Math.sqrt(d)) / (2 * a);
+    x2 = (-b - Math.sqrt(d)) / (2 * a);
+    return [x1, x2];
+  } else if (d === 0) {
+    x1 = -b / (2 * a);
+    return [x1];
+  }
   return arr;
 }
+console.log(solveEquation());
+
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
+  let monthlyPercent = percent / 100 / 12;
+  let mortgageBody = amount - contribution;
+  let monthlyPayment = mortgageBody * (monthlyPercent + (monthlyPercent / (((1 + monthlyPercent) ** countMonths) - 1)))
+  console.log(monthlyPayment)
+  let totalAmount = monthlyPayment * countMonths;
+
+  return Number(totalAmount.toFixed(2));;
 }
+console.log(calculateTotalMortgage());
